8416d840e18b0d60588ff3bd4c4274bba839cb65
Everton Oliveira <contato.evertonoliveira@hotmail.com>
2021-07-26 21:52:51 -0300

    Página Detalhes

--- src/screens/AppointmentsDetails/index.tsx
+++ src/screens/AppointmentsDetails/index.tsx
@@ -1,14 +1,75 @@
 import React from 'react';
-import { View } from 'react-native';
+import { Fontisto } from '@expo/vector-icons';
+import { BorderlessButton } from 'react-native-gesture-handler';
+import { ImageBackground, Text, View, FlatList } from 'react-native';
 import { Background } from '../../components/Background';
+import { ListHeader } from '../../components/ListHeader';
+import { ListDivider } from '../../components/ListDivider';
 import { Header } from '../../components/Header';
+import { Member } from '../../components/Member';
+import { theme } from '../../global/styles/theme';
+import { styles } from './styles';
+import BannerImg from '../../assets/banner.png';
 
 
 export function AppointmentsDetails(){
+  const members = [
+    {
+      id: '1',
+      username: 'Everton',
+      avatar_url: 'https://github.com/contatoevertonoliveira.png',
+      status: 'online'
+    },
+    {
+      id: '2',
+      username: 'Everton',
+      avatar_url: 'https://github.com/contatoevertonoliveira.png',
+      status: 'offline'
+    },
+  ]
+
   return( 
       <Background>
         <Header
           title="Detalhes"
+          action={
+            <BorderlessButton>
+              <Fontisto
+                name="share"
+                size={24}
+                color={theme.colors.primary}
+              />
+            </BorderlessButton>
+          }
+        />
+
+        <ImageBackground 
+          source={BannerImg}
+          style={styles.banner}
+          >
+          <View style={styles.bannerContent}>
+            <Text style={styles.title}>
+              Lendários
+            </Text>
+            <Text style={styles.subtitle}>
+              É hoje que vamos chegar ao challenger sem perder uma partida da m10.
+            </Text>
+          </View>
+        </ImageBackground>
+
+        <ListHeader 
+          title="Jogadores"
+          subtitle="Total 3"
+          />
+
+        <FlatList 
+          data={members}
+          keyExtractor={item => item.id}
+          renderItem={({ item }) => (
+            <Member data={item} />
+          )}
+          ItemSeparatorComponent={() => <ListDivider />}
+          style={styles.members}
         />
       </Background>
   );
